28adc45a353db00a880b28df81a61e8633a2c374
ojuara <lucas.araujo@ambar.tech>
2020-12-26 17:16:10 -0300

    Adiciona estilização para o container dos herois

--- src/components/heroesContainer/index.js
+++ src/components/heroesContainer/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroesHeader from '../heroesHeader';
 import HeroesList from '../heroesList';
 
+import './index.css';
+
 const HeroesContainer = () => {
   return (
-    <>
+    <div className="heroes-container">
       <HeroesHeader />
       <HeroesList />
-    </>
+    </div>
   );
 };
 

45eb1eab66d9def97c7cb42dc3a0259fbfc4b15c
ojuara <lucas.araujo@ambar.tech>
2020-12-26 15:34:24 -0300

    Troca browserrouter por hashrouter para funcionar nos subdominios do github pages

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
       <Switch>
         <Route exact path="/" component={Home} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 

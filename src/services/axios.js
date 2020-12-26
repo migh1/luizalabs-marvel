bb882b50c0d7e5ca0eaae2e461ac28deebefe013
ojuara <lucas.araujo@ambar.tech>
2020-12-26 20:26:25 -0300

    Adiciona stage deploy

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const secure = process.env.NODE_ENV === 'production' ? 's' : '';
+
 const instance = axios.create({
-  baseURL: 'http://gateway.marvel.com',
+  baseURL: `http${secure}://gateway.marvel.com`,
 });
 
 export default instance;

7aadef12b30fea24be27af55bce3e01ed950e6a8
ojuara <lucas.araujo@ambar.tech>
2020-12-26 17:49:40 -0300

    Adiciona o icone de favoritos

---
+++ src/components/favorite/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import favoriteOutlinedIcon from '../../assets/icones/heart/Path Copy 2.png';
+import favoriteFilledIcon from '../../assets/icones/heart/Path.png';
+
+import './index.css';
+
+const Favorite = (active) => {
+  return active ? (
+    <img className="favorite-icon" src={favoriteFilledIcon} alt="Botão de coração preenchido" />
+  ) : (
+    <img
+      className="favorite-icon"
+      src={favoriteOutlinedIcon}
+      alt="Botão de coração não preenchido"
+    />
+  );
+};
+
+export default Favorite;

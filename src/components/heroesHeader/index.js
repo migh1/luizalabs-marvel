fdc453e0f5daed8451780241cb3ae87b021706be
ojuara <lucas.araujo@ambar.tech>
2020-12-26 17:16:30 -0300

    Adiciona estilização para a Header do container de herois

--- src/components/heroesHeader/index.js
+++ src/components/heroesHeader/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import './index.css';
+
 const HeroesHeader = () => {
-  return <>Header</>;
+  return (
+    <div className="heroesHeader-container">
+      <div>
+        <span>Encontrado 20 heróis</span>
+      </div>
+      <div>
+        <span>Ordernar por nome - A/Z</span>
+        <span>Somente favoritos</span>
+      </div>
+    </div>
+  );
 };
 
 export default HeroesHeader;

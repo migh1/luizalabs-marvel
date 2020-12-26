9b52cf4df7ea81fe4178c8c30ec7f3ae165505ef
ojuara <lucas.araujo@ambar.tech>
2020-12-26 15:50:11 -0300

    Componentiza a Logo principal

---
+++ src/components/logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MarvelLogo from '../../assets/logo/Group.png';
+
+const Logo = () => {
+  return <img src={MarvelLogo} alt="Marvel logo" />;
+};
+
+export default Logo;

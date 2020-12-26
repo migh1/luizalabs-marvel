858a98d8dd9870162c73335b318fb7a2a84f74fe
ojuara <lucas.araujo@ambar.tech>
2020-12-26 16:50:24 -0300

    Adiciona estilização para a searchBar

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import './index.css';
+
 const SearchBar = () => {
-  return <input type="search" placeholder="Procure por heróis" />;
+  return (
+    <div className="searchBar-container">
+      <input className="searchBar" type="search" placeholder="Procure por heróis" />
+    </div>
+  );
 };
 
 export default SearchBar;

e5ba1c6a6ebd3ed75b5d3868e2d4e86b6d2ec74c
ojuara <lucas.araujo@ambar.tech>
2020-12-26 17:50:43 -0300

    Melhorias dos estilos da Header do container de herois

--- src/components/heroesHeader/index.js
+++ src/components/heroesHeader/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Toggle from '../toggle';
+import Favorite from '../favorite';
+import nounSuperheroIcon from '../../assets/icones/heroi/noun_Superhero_2227044.png';
 
 import './index.css';
 
@@ -6,11 +9,18 @@ const HeroesHeader = () => {
   return (
     <div className="heroesHeader-container">
       <div>
-        <span>Encontrado 20 heróis</span>
+        <span className="heroes-header-total">Encontrados 20 heróis</span>
       </div>
       <div>
-        <span>Ordernar por nome - A/Z</span>
-        <span>Somente favoritos</span>
+        <span className="heroes-header-color">
+          <img src={nounSuperheroIcon} alt="ícone de herói com capa" />
+          Ordernar por nome - A/Z
+          <Toggle />
+        </span>
+        <span className="heroes-header-color">
+          <Favorite />
+          Somente favoritos
+        </span>
       </div>
     </div>
   );

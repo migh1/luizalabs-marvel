2fd678e1b52a51ac513684e21c9ab50df05418c0
ojuara <lucas.araujo@ambar.tech>
2020-12-27 16:38:14 -0300

    Ajuste no componente de favorito

--- src/components/favorite/index.js
+++ src/components/favorite/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import favoriteOutlinedIcon from '../../assets/icones/heart/Path Copy 2.png';
+import favoriteOutlinedIconx2 from '../../assets/icones/heart/Path Copy 2@2x.png';
 import favoriteFilledIcon from '../../assets/icones/heart/Path.png';
+import favoriteFilledIconx2 from '../../assets/icones/heart/Path@2x.png';
 
 import './index.css';
 
-const Favorite = ({ active }) => {
+const favoriteIcon = {
+  outlined: {
+    x1: favoriteOutlinedIcon,
+    x2: favoriteOutlinedIconx2,
+  },
+  filled: {
+    x1: favoriteFilledIcon,
+    x2: favoriteFilledIconx2,
+  },
+};
+
+const Favorite = ({ active, size }) => {
   return active ? (
-    <img className="favorite-icon" src={favoriteFilledIcon} alt="Botão de coração preenchido" />
+    <img
+      className={`favorite-icon${size || 'x1'}`}
+      src={favoriteIcon.filled[size || 'x1']}
+      alt="Botão de coração preenchido"
+    />
   ) : (
     <img
-      className="favorite-outlined-icon"
-      src={favoriteOutlinedIcon}
+      className={`favorite-outlined-icon${size || 'x1'}`}
+      src={favoriteIcon.outlined[size || 'x1']}
       alt="Botão de coração não preenchido"
     />
   );

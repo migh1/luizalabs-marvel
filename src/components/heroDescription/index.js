423211375d697225799b900f201137d5d3cff9c8
ojuara <lucas.araujo@ambar.tech>
2020-12-27 16:49:16 -0300

    Adicionar descrição e detalhes do heroi

--- src/components/heroDescription/index.js
+++ src/components/heroDescription/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import Favorite from '../favorite';
 
-const HeroDescription = () => {
-  return <></>;
+import './index.css';
+
+const HeroDescription = ({ hero }) => {
+  return (
+    <div className="hero-description-container">
+      <div className="hero-title-container">
+        <div>
+          <span className="hero-title">{hero.name}</span>
+        </div>
+        <div>
+          <Favorite size="x2" />
+        </div>
+      </div>
+
+      <div className="hero-description">
+        <span>{hero.description}</span>
+      </div>
+
+      <div className="hero-add-info">
+        <div className="hero-comics">
+          <span>Quadrinhos</span>
+          <br />
+          <span>3.0000</span>
+        </div>
+
+        <div className="hero-movies">
+          <span>Filmes</span>
+          <br />
+          <span>40</span>
+        </div>
+      </div>
+
+      <div className="hero-rating">
+        <span>Rating: </span>
+      </div>
+
+      <div className="hero-last-comic">
+        <span>Último quadrinho: </span>
+        <span>13 fev. 2020</span>
+      </div>
+    </div>
+  );
 };
 
 export default HeroDescription;

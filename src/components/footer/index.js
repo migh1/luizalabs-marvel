87b32f960b71829de2edec222df948942b82f1f6
ojuara <lucas.araujo@ambar.tech>
2020-12-27 14:19:19 -0300

    Adiciona uma footer vermelha ao final da página

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './index.css';
+
+const Footer = () => {
+  return <div className="hero-footer" />;
+};
+
+export default Footer;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/header';
 import SearchBar from '../../components/searchBar';
 import HeroesContainer from '../../components/heroesContainer';
+import Footer from '../../components/footer';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <SearchBar />
       <HeroesContainer />
+      <Footer />
     </>
   );
 };
